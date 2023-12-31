eeb802ebf4395faeabfaa88f6e4295af453193d9
Andrew Gomes <andrewgomes1328@gmail.com>
2023-12-30 23:45:23 -0300

    rotas feitas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\SerieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('series', [SerieController::class, 'index']);
+Route::post('series', [SerieController::class, 'store']);
+Route::get('series/{id}', [SerieController::class, 'show']);
+Route::put('series/{id}', [SerieController::class, 'update']);
+Route::delete('series/{id}', [SerieController::class, 'destroy']);
