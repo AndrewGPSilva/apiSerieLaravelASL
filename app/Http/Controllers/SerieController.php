fa75de23b4bd68602c9cd4fe70f904447c01e8e2
Andrew Gomes <andrewgomes1328@gmail.com>
2023-12-31 00:01:37 -0300

    controller com CRUD finalizado

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SerieController extends Controller
 {
-    //
+    public function index()
+    {
+        $aulas = Serie::all();
+        return response()->json($aulas);
+    }
+
+    public function store(Request $request)
+    {
+        $aula = Serie::create($request->all());
+        return response()->json($aula);
+    }
+
+    public function show($id)
+    {
+        $aula = Serie::find($id);
+        return response()->json($aula);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $aula = Serie::find($id);
+        $aula->update($request->all());
+        return response()->json($aula);
+    }
+
+    public function destroy($id)
+    {
+        $aula = Serie::find($id);
+        $aula->delete();
+        return response()->json(['message' => 'Serie deletada com sucesso!']);
+    }
 }
